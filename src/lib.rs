801ebefa43045393033d3e860e3e4e8c549ac10b
mryao <mryao@pku.edu.cn>
2022-08-11 18:49:15 +0800

    bump aes-gcm-siv to 0.11.1

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
-use aes_gcm_siv::{Aes128GcmSiv, Key, Nonce};
-use aes_gcm_siv::aead::{Aead, NewAead, Payload};
+use aes_gcm_siv::{
+    aead::{Aead, KeyInit, Payload},
+    Aes128GcmSiv, Nonce,
+};
 use wasm_bindgen::prelude::*;
 
 mod utils;
@@ -12,9 +14,7 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
 pub fn aes128gcm_siv_encrypt(key_slice: &[u8], nonce_slice: &[u8], aad_slice: &[u8], plaintext_slice: &[u8]) -> Vec<u8> {
-    let key = Key::from_slice(key_slice);
-    let cipher = Aes128GcmSiv::new(key);
-
+    let cipher = Aes128GcmSiv::new_from_slice(key_slice).expect("key error");
     let nonce = Nonce::from_slice(nonce_slice);
 
     cipher.encrypt(nonce, Payload { msg: plaintext_slice, aad: aad_slice }).expect("encryption failure!")
@@ -22,9 +22,7 @@ pub fn aes128gcm_siv_encrypt(key_slice: &[u8], nonce_slice: &[u8], aad_slice: &[
 
 #[wasm_bindgen]
 pub fn aes128gcm_siv_decrypt(key_slice: &[u8], nonce_slice: &[u8], aad_slice: &[u8], ciphertext_slice: &[u8]) -> Vec<u8> {
-    let key = Key::from_slice(key_slice);
-    let cipher = Aes128GcmSiv::new(key);
-
+    let cipher = Aes128GcmSiv::new_from_slice(key_slice).expect("key error");
     let nonce = Nonce::from_slice(nonce_slice);
 
     cipher.decrypt(nonce, Payload { msg: ciphertext_slice, aad: aad_slice }).expect("decryption failure!")
