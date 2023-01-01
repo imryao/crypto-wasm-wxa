b17af17f4f5b353945762ced864d5ee97ad05cdd
mryao <mryao@pku.edu.cn>
2023-01-01 19:46:06 +0800

    optimize code for jwk

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,10 @@ use aes_gcm_siv::{
     aead::{Aead, KeyInit, Payload},
     Aes128GcmSiv, Nonce,
 };
-use p256::{EncodedPoint, PublicKey, ecdh::EphemeralSecret, ecdsa::{Signature, VerifyingKey, signature::Verifier}, elliptic_curve::sec1::FromEncodedPoint};
-use rand::{SeedableRng, rngs::StdRng};
+use p256::{ecdh::EphemeralSecret, ecdsa::{signature::{Signature, Verifier}, VerifyingKey}, elliptic_curve::sec1::FromEncodedPoint, EncodedPoint, PublicKey};
+use rand::{rngs::StdRng, SeedableRng};
 use sha2::Sha256;
 use wasm_bindgen::prelude::*;
-
-mod utils;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -17,25 +15,29 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
 pub fn aes128gcm_siv_encrypt(key_slice: &[u8], nonce_slice: &[u8], aad_slice: &[u8], plaintext_slice: &[u8]) -> Vec<u8> {
-    let cipher = Aes128GcmSiv::new_from_slice(key_slice).expect("key error");
+    let cipher = Aes128GcmSiv::new_from_slice(key_slice).unwrap();
     let nonce = Nonce::from_slice(nonce_slice);
 
-    cipher.encrypt(nonce, Payload { msg: plaintext_slice, aad: aad_slice }).expect("encryption failure!")
+    cipher.encrypt(nonce, Payload { msg: plaintext_slice, aad: aad_slice }).unwrap()
 }
 
 #[wasm_bindgen]
 pub fn aes128gcm_siv_decrypt(key_slice: &[u8], nonce_slice: &[u8], aad_slice: &[u8], ciphertext_slice: &[u8]) -> Vec<u8> {
-    let cipher = Aes128GcmSiv::new_from_slice(key_slice).expect("key error");
+    let cipher = Aes128GcmSiv::new_from_slice(key_slice).unwrap();
     let nonce = Nonce::from_slice(nonce_slice);
 
-    cipher.decrypt(nonce, Payload { msg: ciphertext_slice, aad: aad_slice }).expect("decryption failure!")
+    cipher.decrypt(nonce, Payload { msg: ciphertext_slice, aad: aad_slice }).unwrap()
 }
 
 #[wasm_bindgen]
-pub fn generate_secret(seed: &[u8], x: &[u8], y: &[u8], info: &[u8]) -> Vec<u8> {
+pub fn generate_key(seed: &[u8], x: &[u8], y: &[u8], kid: &[u8], sign: &[u8], info: &[u8]) -> Vec<u8> {
     let mut rng = StdRng::from_seed(seed.try_into().unwrap());
 
     let server_point = EncodedPoint::from_affine_coordinates(x.into(), y.into(), false);
+    let msg = [server_point.as_bytes(), kid].concat();
+    if !verify(&msg, sign) {
+        return Vec::new();
+    }
     let server_pk = PublicKey::from_encoded_point(&server_point).unwrap();
 
     let client_secret = EphemeralSecret::random(&mut rng);
@@ -44,13 +46,13 @@ pub fn generate_secret(seed: &[u8], x: &[u8], y: &[u8], info: &[u8]) -> Vec<u8> 
     let shared_secret = client_secret.diffie_hellman(&server_pk);
     let mut okm = [0u8; 16];
     shared_secret.extract::<Sha256>(None).expand(info, &mut okm).unwrap();
-    [&mut okm, client_point.as_bytes()].concat()
+    [&mut okm, client_point.compress().as_bytes()].concat()
 }
 
-#[wasm_bindgen]
-pub fn verify(msg: &[u8], sign_der: &[u8]) -> bool {
-    let verifying_key_bytes = [3, 113, 106, 151, 151, 175, 10, 112, 172, 240, 217, 41, 219, 93, 237, 150, 152, 249, 238, 67, 171, 110, 162, 90, 91, 168, 178, 75, 22, 243, 219, 65, 232];
-    let verifying_key = VerifyingKey::from_sec1_bytes(&verifying_key_bytes).unwrap();
-    let signature = Signature::from_der(sign_der).unwrap();
+const VERIFYING_KEY_BYTES: [u8; 33] = [2, 163, 219, 94, 18, 26, 5, 230, 207, 241, 159, 187, 184, 166, 85, 40, 133, 179, 229, 201, 41, 253, 31, 121, 77, 96, 131, 125, 220, 200, 41, 245, 11];
+
+fn verify(msg: &[u8], sign: &[u8]) -> bool {
+    let verifying_key = VerifyingKey::from_sec1_bytes(&VERIFYING_KEY_BYTES).unwrap();
+    let signature = Signature::from_bytes(sign).unwrap();
     verifying_key.verify(msg, &signature).is_ok()
 }
--- tests/web.rs
+++ tests/web.rs
@@ -3,11 +3,40 @@
 #![cfg(target_arch = "wasm32")]
 
 extern crate wasm_bindgen_test;
+
+use base64ct::{Base64, Base64UrlUnpadded, Encoding};
 use wasm_bindgen_test::*;
+
+use crypto_wasm_wxa::{generate_key, verify};
 
 wasm_bindgen_test_configure!(run_in_browser);
 
 #[wasm_bindgen_test]
-fn pass() {
-    assert_eq!(1 + 1, 2);
+fn verify_test() {
+    let result = verify("eyJhbGciOiJFUzI1NiJ9.SGVsbG8sIHdvcmxkIQ".as_bytes(), &Base64UrlUnpadded::decode_vec("dAfO1PfJdz64YY4NYB9dyPn12JjtG2whiDS1DbZyZyF9QeTl568Fid1080pPnJqY_gntlo-tWdbqDu26GtkRJQ").unwrap());
+    assert_eq!(result, true)
+}
+
+#[wasm_bindgen_test]
+fn generate_secret_test() {
+    const SEED: [u8; 32] = [7u8; 32];
+    const X_STR: &str = "a8Q9Vsljg_d0hvykfAlPVX7CcfEb4nIBKD3Y8qHpyJM";
+    const Y_STR: &str = "SK0WtyEsA_M_qH-Gs5MoVGTnjSJgECN7KUmZKJ4sY8o";
+    const KID_STR: &str = "2023-01-01";
+    const SIGN_STR: &str = "h9N9AfUoUQXWZ2lUEgdS1IxoyiPpl1m9WyHcSnI7OPdqSqWvrjlt_l0lD85VGWxAFEDkGf-jY-T4I20PotUt-g";
+    const INFO_STR: &str = "wxebbb8eded25ba277";
+
+    let x = Base64UrlUnpadded::decode_vec(X_STR).unwrap();
+    let y = Base64UrlUnpadded::decode_vec(Y_STR).unwrap();
+    let kid = KID_STR.as_bytes();
+    let sign = Base64UrlUnpadded::decode_vec(SIGN_STR).unwrap();
+    let info = INFO_STR.as_bytes();
+
+    let result = generate_key(&SEED, &x, &y, kid, &sign, info);
+    let key = &result[..16];
+    let pk = &result[16..];
+    println!("{}", Base64::encode_string(key));
+    println!("{}", Base64::encode_string(pk));
+
+    assert_eq!(true, true)
 }
